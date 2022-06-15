271b66576ce122ee301db91dc7182d6ed6adabc6
Isaiah <isaiahpatton1@gmail.com>
2022-06-15 08:35:32 -0400

    Download page now use Modrinth's API

--- download/index-js.js
+++ download/index-js.js
@@ -19,7 +19,35 @@ function apex() {
 setInterval(apex, 5000);
 apex();
 
+
+function req_new(ver, job) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+
+			var name = myObj[0].name.split(" ")[0].replace('#','')
+			var dl = myObj[0].files[0].url
+			var date = new Date(myObj[0].date_published)
+			var formattedTime = date.toString().split(" ")[1] + " " + date.getDate() + ", " + date.getFullYear();
+
+			document.getElementById("txt-" + ver).innerHTML = '<span class="tspan">Download </a> #' + name + "&nbsp;<b>(for 1.18.2)</b></span>";
+			document.getElementById("dl-" + ver).href = dl;
+
+			var log = "";
+			document.getElementById("info-" + ver).innerHTML = "Last update on "+formattedTime+" "+log;
+		}
+	};
+	xmlhttp.open("GET", "https://api.modrinth.com/v2/project/cardboard/version", true);
+	xmlhttp.send();
+}
+
 function req(ver, job) {
+	if (ver == "17") {
+		req_new(ver, job)
+		return
+	}
+	
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
